25251a2e209919e7d45f0f90f4ca2dd1aaaccb12
Nicholas Clucas <nclucas01@gmail.com>
2018-08-17 00:52:30 -0400

    capitalize and sort stuff

---
+++ app/helpers/capitalize.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export function capitalize([string]) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export default helper(capitalize);
